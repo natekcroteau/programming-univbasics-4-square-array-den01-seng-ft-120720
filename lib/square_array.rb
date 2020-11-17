ac475a0b492fb64c54d22135a8644d9b2cd3f4e7
Nate Croteau <natekcroteau@gmail.com>
2020-11-17 21:36:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
 counter = 0 
-  return_array = []
   while counter < array.length do
-    return_array.push(array[counter]*array[counter])
-    counter +=1 
+  puts array[counter]*array[counter]
+  counter +=1 
   end    
-  return_array
 end
